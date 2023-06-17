f631e099ad76309bb2954f5632078790ba2c6222
leesemin <leesemin222@gmail.com>
2023-06-17 16:54:51 +0900

    next.js 설치하려했지만 webpack babel설치 안해도 된다해서 일단 express를 설치하여 개발환경완성에 초점을 둠

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const webpack =require('webpack')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const config = {
+  entry : path.resolve(__dirname, "src",'index.tsx'),
+  output : {path:path.resolve(__dirname,'dist')},
+  mode : isProduction ? "production" : "development",
+  plugin : [
+    new HtmlWebpackPlugin({
+      template : path.resolve(__dirname, "src","index.html"),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      React : "react"
+    }),
+  ],
+  module : {
+    rules: [
+      {
+        test : /\.(tsx?|js)$/,
+        exclude : /node_modules/,
+        use : ["babel-loader","ts-loader"],
+      },
+      {
+        test : /\.css$/i,
+        use : ["style-loader",'css-loader']
+      },
+      {
+        test : /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type : "asset"
+      },
+    ],
+  },
+  resolve : {
+    extensions : [".tsx",".ts",".jsx",".js"],
+  },
+  optimization:{
+    minimizer:[
+      new TerserPlugin({
+        terserOptions:{
+          compress : {
+            drop_console : true //콘솔로그 제거
+          },
+          output : {
+            comments : true, //주석제거
+          },
+        },
+      }),
+    ],
+  },
+  devServer : {
+    historyApiCallback : true,
+    port : 8080,
+    hot : true,
+    inline : true,
+    publicPath : "/",
+    open: true,
+  }
+}
+
+module.exports = config;
