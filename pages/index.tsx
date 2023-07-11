d61fa8850e0437211488fb0491bac728af5c8d84
BlahBlah <leesemin222@gmail.com>
2023-07-11 16:44:39 +0900

    함수 이름 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-import MA1920 from "../public/img/—Pngtree—pure watercolor gradient colorful background_964413.jpg";
 import React from "react";
 
-const StartImg = () => {
-  return (
-    <div>
-      <Image alt="수체화" src={MA1920} className="height-100, rotate-90" />
-    </div>
-  );
+const Index = () => {
+  return <div className="backgroundImage, height-100, width-100"></div>;
 };
 
-export default StartImg;
+export default Index;
