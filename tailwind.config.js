b61e4e8b54a5148ecd0631a315a1c172927c2b6b
leesemin <leesemin222@gmail.com>
2023-07-13 23:23:49 +0900

    CSS 변수로 시그니처 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const signitureColor = {
+  "--signiture-color-red": "#F38181",
+  "--signiture-color-green": "#EAFFD0",
+  "--signiture-color-blue": "#95E1D3",
+  "--signiture-color-yellow": "#FCE38A",
+};
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundColor: signitureColor,
     },
   },
   plugins: [],
-}
+};
