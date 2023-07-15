14e7c0f7a12314ef830ef539090db739b8264aab
leesemin <leesemin222@gmail.com>
2023-07-15 19:45:03 +0900

    4분기로 나누고 컬러와 블록넘버를 입력하면 나오게끔 만들려했다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import Block, { BlockStyle } from "../components/block";
+import Block from "../components/block";
 
 const StartImg = () => {
-  const firstBlock: BlockStyle = {
-    container: "grid grid-cols-2 blockContainer m-10",
-    first: "upperBlockStyle  z-[0] bg-signiture-color-red",
-    blockCase: { 2: "bg-[#ffffff] rounded-ee-[20px] z-[1] upperBlockStyle" },
-    second: "bg-signiture-color-red innerBlockStyle rounded-t-[25px] ml-[25px]",
-    third: "bg-signiture-color-red blockStyle rounded-s-[25px]",
-    fourth: "bg-signiture-color-red blockStyle rounded-ee-[25px]",
-  };
   return (
-    <>
-      <Block></Block>
-    </>
+    <div style={{ width: 1000, height: 1000 }}>
+      <Block blockNumber="first" colorStyle="signiture-color-red"></Block>
+    </div>
   );
 };
 
